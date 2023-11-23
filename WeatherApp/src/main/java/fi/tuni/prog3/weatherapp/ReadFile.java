54be9fcd73aa86203c7997870951e576cc921885
Oona Leivo <oonamarialeivo@gmail.com>
2023-11-23 12:26:26 +0200

    lisää readfile toteutus

--- WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/ReadFile.java
+++ WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/ReadFile.java
@@ -4,10 +4,36 @@
  */
 package fi.tuni.prog3.weatherapp;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
 /**
  *
  * @author oonam
  */
-public class ReadFile {
-    
+public class ReadFile implements iReadAndWriteToFile {
+
+    private String dataToWrite; // Data to be written to the file
+
+    public void setDataToWrite(String data) {
+        this.dataToWrite = data;
+    }
+
+    @Override
+    public String readFromFile(String fileName) throws IOException {
+        Path filePath = Path.of(fileName);
+        return Files.readString(filePath);
+    }
+
+    @Override
+    public boolean writeToFile(String fileName) throws IOException {
+        if (dataToWrite == null) {
+            throw new IllegalStateException("No data set to write.");
+        }
+        Path filePath = Path.of(fileName);
+        Files.writeString(filePath, dataToWrite, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+        return true;
+    }
 }
