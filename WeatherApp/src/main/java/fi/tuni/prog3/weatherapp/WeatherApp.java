8e985d3992a65c14ea5ac3f55d4eacb64ef5aa1a
Oona Leivo <oonamarialeivo@gmail.com>
2023-11-27 11:18:11 +0200

    lisätty viimeksi haetun kaupungin tallennus, korjattu hakuikkunan ohjetekstitulostuksia

--- WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/WeatherApp.java
+++ WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/WeatherApp.java
@@ -44,7 +44,7 @@ import javafx.stage.Stage;
 // koodeja pitää jaotella useampiin ryhmiin
 // päiväkohtainen ennuste sectio pitää tehdä
 // tuntikohtainen ennuste sectio pitää tehdä
-// pitää tallentaa viimesin haettu kaupunki
+// pitää tallentaa viimesin haettu kaupunki - tehty!
 // pitää asettaa rain 0 jos null
 // kuvat rain, wind, humidity ?
 // yksikkötestit
@@ -67,8 +67,12 @@ public class WeatherApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         // get the weather
+        String lastSearchedCity = loadLastSearchedCity();
+        if (lastSearchedCity == null || lastSearchedCity.isEmpty()) {
+            lastSearchedCity = "Tampere"; // Replace with a default city name
+        }
         weatherApiImpl  weatherApi = new  weatherApiImpl();
-        weatherApi.lookUpLocation("Joensuu");  
+        weatherApi.lookUpLocation(lastSearchedCity);  
         ReadFile file = new ReadFile();
         file.readFromFile("weatherData");
         weather = file.getWeather();
@@ -183,7 +187,28 @@ public class WeatherApp extends Application {
 
         return dailySection;
     }
-       
+    
+        private void saveLastSearchedCity(String cityName) {
+        try {
+            Path filePath = Paths.get("lastSearchedCity.txt");
+            Files.writeString(filePath, cityName);
+        } catch (IOException e) {
+            e.printStackTrace(); // Handle exception
+        }
+    }
+        
+    private String loadLastSearchedCity() {
+        try {
+            Path filePath = Paths.get("lastSearchedCity.txt");
+            if (Files.exists(filePath)) {
+                return Files.readString(filePath).trim();
+            }
+        } catch (IOException e) {
+            e.printStackTrace(); // Handle exception
+        }
+        return null;
+    }
+
     private ImageView getIcon() {
        int weatherCode = weather.getWeatherCode();
           
@@ -267,16 +292,24 @@ public class WeatherApp extends Application {
         Button searchButton = new Button("Search");
         searchButton.setStyle("-fx-background-color: #FFC0CB; -fx-border-color: #8B008B; -fx-background-radius: 10;"); // Pastel Pink with dark pink border and rounded corners
         searchButton.setOnAction(e -> {
-            performSearch(cityTextField.getText());
-            searchStage.close();
+            String cityName = cityTextField.getText().trim(); // Trim to remove leading and trailing whitespace
+            if (!cityName.isEmpty()) {
+                performSearch(cityName);
+                secondWindowInfoText.setText("");
+                searchStage.close();
+            } else {
+                // Optional: Display a message or log that no city name was entered
+                System.out.println("No city name entered."); // Or update a UI element with this message
+                secondWindowInfoText.setText("Please type a city name.");
+            }
         });
-
+        
         Button addToFavoritesButton = new Button("Add to Favorites");
         addToFavoritesButton.setStyle("-fx-background-color: #FFC0CB; -fx-border-color: #8B008B; -fx-background-radius: 10;"); // Pastel Yellow with dark yellow border and rounded corners
         addToFavoritesButton.setOnAction(e -> {
             String cityName = cityTextField.getText();
             if (cityName.isEmpty()) {
-                secondWindowInfoText.setText("Please type a city name.");
+                Platform.runLater(() -> secondWindowInfoText.setText("Please type a city name."));
             } else {
                 addToFavorites(cityName);
                 secondWindowInfoText.setText("Favorite successfully saved!");
@@ -313,6 +346,9 @@ public class WeatherApp extends Application {
         searchStage.setTitle("City Search & Favorites:");
         
          Platform.runLater(() -> mainLayout.requestFocus());
+    } else {
+        // Clear the text field if the window already exists
+        cityTextField.setText("");
     }
     // Show the stage without re-creating the UI components
     searchStage.show();
@@ -328,7 +364,8 @@ public class WeatherApp extends Application {
                 favoritesMenu.getItems().add(menuItem);
             }
         } catch (IOException e) {
-            e.printStackTrace(); // Handle exception
+            // Handle exception
+            
         }
     }
     
@@ -355,7 +392,6 @@ private void addToFavorites(String cityName) {
             Platform.runLater(() -> secondWindowInfoText.setText("City is already a favorite."));
         }
     } catch (IOException ex) {
-        ex.printStackTrace();
         Platform.runLater(() -> secondWindowInfoText.setText("Error: " + ex.getMessage()));
     }
 }
@@ -371,6 +407,8 @@ private void addToFavorites(String cityName) {
             Platform.runLater(this::updateCurrentWeatherSection);
         } catch (IOException e) {
         }
+        
+        saveLastSearchedCity(cityName);
     }
 
         public static void main(String[] args) {
